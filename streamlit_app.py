af57ad3ddce278bfb30c99d2ef2c2f678e296be6
Prakhar <114463825+prakhar211999@users.noreply.github.com>
2022-09-27 13:13:34 +0530

    Update streamlit_app.py
    Added MultiSelect Option on Field Fruit Name

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,9 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+
+my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.multiselect("Pick Some Fruits:",list(my_fruit_list))
+
 streamlit.dataframe(my_fruit_list)
 
